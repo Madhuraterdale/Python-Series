1b2585208fe47578e0e13604f4f53ac9f1ca5261
Madhura <madhuraterdale47@gmail.com>
2025-12-25 13:33:20 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,19 @@
+#Prnt the sum by taking the input from user
+a=int(input("Enter first no. :"))
+b=int(input("Enter second no. :"))
+sum=a+b
+print("Addition of two numbers: ",sum)
+
+#print the area of square
+a=int(input("Enter the length of side:"))
+print("Area of square is: ",a**2)
+
+#average of the floating number
+m=float(input("Enter 1st no. :"))
+p=float(input("Enter 2nd no. :"))
+n=(m+p)/2
+print("Average of floating no. :",n)
+
+a=int(input( "1st no.:"))
+b=int(input("2nd no. :"))
+print(a>=b)
---
+++ input.py
@@ -0,0 +1,14 @@
+#Basic Input Taking in Python
+"""name=input("Enter your name:")
+print("Welcome ",name)
+
+age=int(input("Enter your age:"))
+print(type(age))"""
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+CGPA= float(input("Enter your CGPA:"))
+
+print("Welcome, ",name)
+print("You entered your age as:",age)
+print("Your score is:",CGPA)
